8835593e99a1395b5b05849cc3ddda988fe1d121
LimeByte <limebyte@twf.co.nz>
2012-08-06 20:59:54 +1200

    Added a PaperBlockDesign.

--- src/main/java/me/limebyte/slender/PaperBlock.java
+++ src/main/java/me/limebyte/slender/PaperBlock.java
@@ -10,16 +10,6 @@ import org.getspout.spoutapi.sound.SoundEffect;
  * Represents a paper block.
  */
 public class PaperBlock extends GenericCuboidCustomBlock {
-
-	/**
-	 * The size of the texture image in pixels.
-	 */
-	private static final int TEXTURE_SIZE = 16;
-	
-	/**
-	 * The depth of the block.
-	 */
-	private static final float BLOCK_DEPTH = 0.05F;
 	
 	/**
 	 * Creates a new place-able piece of paper.
@@ -27,14 +17,21 @@ public class PaperBlock extends GenericCuboidCustomBlock {
 	 * @param plugin The JavaPlugin making this block
 	 */
 	public PaperBlock(final Plugin plugin) {
-		super(plugin,
-			  "PaperBlock",
-			  Slender.TEXTURE_URL + "note.png", TEXTURE_SIZE,
-			  0F, 0F, 0F, 1F, 1F, BLOCK_DEPTH, false);
-		
+		super(plugin, "Paper Block", true);
+		this.setBlockDesign(new PaperBlockDesign(plugin));
 		this.setItemDrop(new ItemStack(Material.PAPER, 1));
-		this.setOpaque(true);
 		this.setStepSound(SoundEffect.CLOTH);
 	}
+	
+//	public PaperBlock(final Plugin plugin) {
+//		super(plugin,
+//			  "Paper Block",
+//			  Slender.TEXTURE_URL + "note.png", TEXTURE_SIZE,
+//			  0F, 0F, 0F, 1F, 1F, BLOCK_DEPTH, false);
+//		
+//		this.setItemDrop(new ItemStack(Material.PAPER, 1));
+//		this.setOpaque(true);
+//		this.setStepSound(SoundEffect.CLOTH);
+//	}
 
 }
---
+++ src/main/java/me/limebyte/slender/PaperBlockDesign.java
@@ -0,0 +1,47 @@
+package me.limebyte.slender;
+
+import org.bukkit.plugin.Plugin;
+import org.getspout.spoutapi.block.design.GenericCuboidBlockDesign;
+import org.getspout.spoutapi.block.design.Texture;
+
+/**
+ * Represents a paper block design.
+ */
+public class PaperBlockDesign extends GenericCuboidBlockDesign {
+
+	/**
+	 * The depth of the block.
+	 */
+	private static final float BLOCK_DEPTH = 0.05F;
+	
+	/**
+	 * The size of the individual texture in pixels.
+	 */
+	private static final int TEXTURE_SIZE = 16;
+	
+	/**
+	 * The size of the texture file in pixels.
+	 */
+	private static final int TEXTURE_WIDTH = 32;
+	
+	/**
+	 * The size of the texture file in pixels.
+	 */
+	private static final int TEXTURE_HEIGHT = 16;
+	
+	/**
+	 * Creates a new PaperBlock design.
+	 * 
+	 * @param plugin The JavaPlugin making this design
+	 */
+	public PaperBlockDesign(final Plugin plugin) {
+		super(plugin,
+			  new Texture(
+					  plugin, Slender.TEXTURE_URL + "note.png",
+					  TEXTURE_WIDTH, TEXTURE_HEIGHT, TEXTURE_SIZE),
+			  new int[]{0, 1, 1, 1, 1, 1},
+			  0F, 0F, 0F, 1F, 1F, BLOCK_DEPTH);
+		this.setBoundingBox(0F, 0F, 0F, 1F, 1F, BLOCK_DEPTH);
+	}
+
+}
