a81e8fa49d0438c032b73e723abc5e2275c6294b
LimeByte <limebyte@twf.co.nz>
2012-08-07 23:13:02 +1200

    Lots of fixes for PaperBlock.

--- src/main/java/me/limebyte/slender/PaperBlock.java
+++ src/main/java/me/limebyte/slender/PaperBlock.java
@@ -1,10 +1,12 @@
 package me.limebyte.slender;
 
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.World;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.getspout.spoutapi.material.MaterialData;
 import org.getspout.spoutapi.material.block.GenericCustomBlock;
-import org.getspout.spoutapi.sound.SoundEffect;
+import org.getspout.spoutapi.player.SpoutPlayer;
 
 /**
  * Represents a paper block.
@@ -14,16 +16,37 @@ public class PaperBlock extends GenericCustomBlock {
 	/**
 	 * The block this block is based off.
 	 */
-	private static final int BASE_BLOCK = 68;
+	private static final int BASE_BLOCK = 53;
 	
 	/**
 	 * Creates a new place-able piece of paper.
 	 */
 	public PaperBlock() {
-		super(Slender.getInstance(), "Paper Block", BASE_BLOCK, true);
-		setBlockDesign(new PaperBlockDesign(0)).setOpaque(false);
-		setHardness(MaterialData.sugarCaneBlock.getHardness());
-		setItemDrop(new ItemStack(Material.PAPER, 1)).setStepSound(SoundEffect.CLOTH);
+		super(Slender.getInstance(), "Paper Block", false);
+		this.setBlockDesign(new PaperBlockDesign(0)).setHardness(MaterialData.sugarCaneBlock.getHardness());
 	}
 
+    public void onNeighborBlockChange(World world, int x, int y, int z, int changedId) { }
+    
+    public void onBlockPlace(World world, int x, int y, int z) { }
+ 
+    public void onBlockPlace(World world, int x, int y, int z, LivingEntity living) { }
+ 
+    public void onBlockDestroyed(World world, int x, int y, int z) { }
+ 
+    public boolean onBlockInteract(World world, int x, int y, int z, SpoutPlayer player) {
+        return true;
+    }
+ 
+    public void onEntityMoveAt(World world, int x, int y, int z, Entity entity) { }
+ 
+    public void onBlockClicked(World world, int x, int y, int z, SpoutPlayer player) { }
+ 
+    public boolean isProvidingPowerTo(World world, int x, int y, int z, BlockFace face) {
+        return false;
+    }
+ 
+    public boolean isIndirectlyProvidingPowerTo(World world, int x, int y, int z, BlockFace face) {
+        return false;
+    }
 }
--- src/main/java/me/limebyte/slender/PaperBlockDesign.java
+++ src/main/java/me/limebyte/slender/PaperBlockDesign.java
@@ -3,6 +3,7 @@ package me.limebyte.slender;
 import org.getspout.spoutapi.block.design.GenericBlockDesign;
 import org.getspout.spoutapi.block.design.Quad;
 import org.getspout.spoutapi.block.design.SubTexture;
+import org.getspout.spoutapi.block.design.Texture;
 
 /**
  * Represents a paper block design.
@@ -15,44 +16,28 @@ public class PaperBlockDesign extends GenericBlockDesign {
 	private static final float BLOCK_DEPTH = 0.1F;
 	
 	/**
-	 * The width of the block.
-	 */
-	private static final float BLOCK_WIDTH = 0.5F;
-	
-	/**
-	 * The height of the block.
-	 */
-	private static final float BLOCK_HEIGHT = 0.8F;
-	
-	/**
-	 * The calculated vertical bounding box margin.
-	 */
-	private static final float VERT_MARGIN = (1F - BLOCK_HEIGHT) / 2F;
-	
-	/**
-	 * The calculated horizontal bounding box margin.
-	 */
-	private static final float HORZ_MARGIN = (1F - BLOCK_WIDTH) / 2F;
-	
-	/**
 	 * Creates a new PaperBlock design.
 	 * 
 	 * @param blockTextureLocation The texture position on the Block texture image
 	 */
 	public PaperBlockDesign(final int blockTextureLocation) {
-		setRenderPass(1);
-		setBoundingBox(1F - BLOCK_DEPTH, VERT_MARGIN, HORZ_MARGIN,
-				            1F, 1F - VERT_MARGIN, 1F - HORZ_MARGIN);
+		Slender plugin = Slender.getInstance();
+		Texture texture = Textures.BLOCKS;
 		
-		SubTexture subTex = Textures.BLOCKS.getSubTexture(blockTextureLocation);
+		setQuadNumber(1);
+		
+		setTexture(plugin, texture.getTexture());
+		setMinBrightness(1F).setMaxBrightness(1F);
+		
+		SubTexture subTex = texture.getSubTexture(blockTextureLocation);
 		
 		Quad frontFace = new Quad(0, subTex);
-		frontFace.addVertex(0, 1F - BLOCK_DEPTH, VERT_MARGIN, HORZ_MARGIN);
-		frontFace.addVertex(1, 1F - BLOCK_DEPTH, VERT_MARGIN, 1F - HORZ_MARGIN);
-		frontFace.addVertex(2, 1F - BLOCK_DEPTH, 1F - VERT_MARGIN, HORZ_MARGIN);
-		frontFace.addVertex(3, 1F - BLOCK_DEPTH, 1F - VERT_MARGIN, 1F - HORZ_MARGIN);
+		frontFace.addVertex(0, 1F, 0F, BLOCK_DEPTH);
+		frontFace.addVertex(1, 1F, 1F, BLOCK_DEPTH);
+		frontFace.addVertex(2, 0F, 1F, BLOCK_DEPTH);
+		frontFace.addVertex(3, 0F, 0F, BLOCK_DEPTH);
 		
-		setQuadNumber(1).setQuad(frontFace);
+	    setQuad(frontFace);
 	}
 
 }
